14e8c9ff58f6b3027c887b86837de916b21cfb41
github-AKZhuk <lesha.zhuk@gmail.com>
2021-04-04 13:00:04 +0300

    feat: add alternative text for img-tag according to rule 1.2

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className = "button button-delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "button-delete__img";
+    deleteButtonImg.setAttribute("alt", "remove");
     deleteButton.appendChild(deleteButtonImg);
 
 
